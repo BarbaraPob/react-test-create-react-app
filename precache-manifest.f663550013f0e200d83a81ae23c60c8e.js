db676855906500f8e8f4951f78e751c4adaeb73d
Mrozek Barbara <barbara.mrozek@tieto.com>
2018-10-12 08:55:17 +0200

    Updates

---
+++ precache-manifest.f663550013f0e200d83a81ae23c60c8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "497feab12376247d368d",
+    "url": "/react-test-create-react-app/static/js/runtime~main.497feab1.js"
+  },
+  {
+    "revision": "04a716dd8439e32b71ed",
+    "url": "/react-test-create-react-app/static/js/main.04a716dd.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/react-test-create-react-app/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "04a716dd8439e32b71ed",
+    "url": "/react-test-create-react-app/static/css/main.35ef6549.chunk.css"
+  },
+  {
+    "revision": "1d2c89c173e794bfe6cb55e622b056f5",
+    "url": "/react-test-create-react-app/index.html"
+  }
+];
